06c56428cf263c221af4f63d9f1476c168a8887b
Morris Tichy <0064@htl.rennweg.at>
2023-09-18 15:27:33 +0200

    Start

---
+++ src/Firsttry/word_countTest.java
@@ -0,0 +1,35 @@
+package Firsttry;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class word_countTest {
+    @Test
+     void easy () {
+        // leicht
+        // leicht
+        assertEquals(0, wordcount.count(""));
+        assertEquals(0, wordcount.count(" "));
+        assertEquals(0, wordcount.count("   "));
+    }
+    @Test
+    void normal(){
+        assertEquals(1, wordcount.count("eins"));
+        assertEquals(1, wordcount.count(" eins"));
+        assertEquals(1, wordcount.count("eins "));
+        assertEquals(1, wordcount.count(" eins "));
+        assertEquals(1, wordcount.count(" eins  "));
+        assertEquals(1, wordcount.count("  eins "));
+        assertEquals(1, wordcount.count("  eins  "));
+        assertEquals(1, wordcount.count("eins:"));
+        assertEquals(1, wordcount.count(":eins"));
+        assertEquals(1, wordcount.count(":eins:"));
+        assertEquals(1, wordcount.count(" eins  "));
+        assertEquals(1, wordcount.count(" eins : "));
+        assertEquals(1, wordcount.count(": eins :"));
+        assertEquals(3, wordcount.count("ein erster Text"));
+        assertEquals(3, wordcount.count(" ein  erster   Text"));
+        assertEquals(3, wordcount.count("ein:erster.Text"));
+    }
+}
