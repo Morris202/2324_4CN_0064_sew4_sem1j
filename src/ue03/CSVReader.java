bbdd4305a2763d218e34f93bbe49fa16656c1c4a
Morris Tichy <0064@htl.rennweg.at>
2023-10-09 16:22:12 +0200

    CSVReader 3.1 jz mit Enum erstes Falsch

--- src/ue03/CSVReader.java
+++ src/ue03/CSVReader.java
@@ -1,13 +1,58 @@
 package ue03;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVReader {
-     String csvstring;
+    String csvstring;
+    List<String>results = new ArrayList<>();
+
+    String puffer = "";
+     enum State {
+         NOWORD{
+             State handleChar(char c, CSVReader reader) {
+                 if (Character.isLetter(c) || Character.isDigit(c)) {
+                     reader.results.add(reader.puffer);
+                     reader.puffer = "";
+                     reader.puffer+=c;
+                     return INWORD;
+                 }
+                 else {
+                     return NOWORD;
+                 }
+             }
+         },
+         INWORD {
+             State handleChar(char c, CSVReader reader) {
+                 if (c == ',') {
+                     return NOWORD;
+                 }
+                 else {
+                     reader.puffer+=c;
+                     return INWORD;
+
+                 }
+             }
+         };
+         abstract State handleChar(char c, CSVReader reader);
+    }
 
     public CSVReader(String csvstring) {
         this.csvstring = csvstring;
     }
-
-    public String[] split() {
-        return this.csvstring.split(",");
+    public List<String> split(String text) {
+        State state = State.INWORD;
+        results = new ArrayList<>();
+        for (char c : text.toCharArray()) {
+            state = state.handleChar(c, this);
+            System.out.println(c);
+        }
+        if (state == state.INWORD){
+            results.add(puffer);
+        }
+        return results;
     }
+
+
+
 }
