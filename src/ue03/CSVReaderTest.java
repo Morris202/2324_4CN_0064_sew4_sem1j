e837fe62e49c3940fa3b42557858ef682269bb5e
Morris Tichy <0064@htl.rennweg.at>
2023-10-09 16:22:33 +0200

    CSVReader 3.1 2 Testfälle

--- src/ue03/CSVReaderTest.java
+++ src/ue03/CSVReaderTest.java
@@ -3,14 +3,26 @@ package ue03;
 import Firsttry.wordcount;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CSVReaderTest {
     @Test
     public void testSplitWithComma() {
-        String csvString = "John,Doe,30,New York";
+        String csvString = "John,Doe,New York";
         CSVReader reader = new CSVReader(csvString);
-        String[] expected = {"John", "Doe", "30", "New York"};
-        assertArrayEquals(expected, reader.split());
+        List<String> expected = List.of("John", "Doe", "New York");
+        List<String> result = reader.split(csvString);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testSplitWithSemicolonSeparator() {
+        String csvString = "Alice,Smith,25,Los Angeles";
+        CSVReader reader = new CSVReader(csvString);
+        List<String> expected = List.of("Alice", "Smith","25","Los Angeles");
+        List<String> result = reader.split(csvString);
+        assertEquals(expected, result);
     }
 }
